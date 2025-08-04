c3e0a1a7d769c43b8b537e9a03410e2dc462ecc7
Swayam <subinghi22@gmail.com>
2025-08-04 13:50:04 +0545

    Putting 80 records in transaction table

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -17,19 +17,11 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
-        static $month = 1;
-        static $year = 2024;
-        $date = Carbon::create($year, $month, 4);
 
-        $month++;
-        if($month > 12) {
-            $year++;
-            $month = 1;
-        }
         return [
-            'amount' => fake()->randomFloat(2, 0, 100000),// randomFloat(decimaldigits, minimum, maximum)
+            'amount' => fake()->randomFloat(2, 0, 9000),// randomFloat(decimaldigits, minimum, maximum)
             'description' => fake()->paragraph(3), 
-            'transaction_date' => $date, 
+            'transaction_date' => fake()->dateTimeBetween('2024-01-01', '2025-08-04')->format('Y-m-d'), 
             'user_id' => 1, 
             'category_id' => fake()->numberBetween(1,9),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // CategoriesSeeder::class,
+            CategoriesSeeder::class,
             IncomeSeeder::class,
             TransactionSeeder::class,
         ]);
